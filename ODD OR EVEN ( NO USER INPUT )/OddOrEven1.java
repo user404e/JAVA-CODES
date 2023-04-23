9423b758134d65cf5a60879965080b1f5f16071f
user404e <131599479+user404e@users.noreply.github.com>
2023-04-23 14:20:02 +0000

    Create OddOrEven1.java

---
+++ ODD OR EVEN ( NO USER INPUT )/OddOrEven1.java
@@ -0,0 +1,14 @@
+// check the number is odd or even
+
+public class OddOrEven1 {
+    public static void main(String[] args) {
+        int num = 12;
+        
+        if (num%2==0){
+            System.out.println("Number Is Even");
+        }
+        else{
+            System.out.println("Number Is Odd");
+        }
+ }   
+}
