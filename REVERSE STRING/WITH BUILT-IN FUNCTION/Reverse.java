7697b63266af8dbb43ea805346841a454a6ea04f
user404e <e404user@gmail.com>
2023-05-13 11:45:09 +0000

    JAVA

---
+++ REVERSE STRING/WITH BUILT-IN FUNCTION/Reverse.java
@@ -0,0 +1,9 @@
+
+
+public class Reverse {
+    public static void main(String[] args) {
+        StringBuilder s1 = new StringBuilder("Hello World");
+
+        System.out.println(s1.reverse());
+    }
+}
